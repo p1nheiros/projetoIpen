1ca70b5016e57830b454036dd7aa239094e15bb1
oliveeiralucas <lucas.oliveira@edu.unifil.br>
2023-06-01 17:50:53 -0300

    rodapé concluído

---
+++ ipen/src/components/Footer/Copyright.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Copyright = () => {
+  return (
+    <div>
+      <p className="absolute bottom-0 w-screen mb-10 text-white text-center ">
+        © 2023 IPEN - Instituto de Pediatria e Neurologia Infantil
+      </p>
+    </div>
+  );
+};
+
+export default Copyright;
