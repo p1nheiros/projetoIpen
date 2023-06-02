0fe3932f0f97d64422914065b4cd180dae6e12fb
p1nheiros <pinheiros.dev@gmail.com>
2023-06-02 17:40:52 -0300

    atualização da pag inicial

--- ipen/src/components/Navbar.jsx
+++ ipen/src/components/Navbar.jsx
@@ -10,11 +10,12 @@ export const Navbar = () => {
   }
 
   return (
-    <div className='flex justify-between items-center h-24 max-w-screen mx-auto px-4 text-white fixed top-0 left-0 right-0 bg-darkGreen font-poppins'>
+    <div className='flex justify-between items-center h-24 max-w-screen mx-auto px-10 text-white fixed top-0 left-0 right-0 bg-darkGreen font-poppins'>
       <h1 className='w-full text-3xl font-bold text-lightGreen'>
         LOGO
       </h1>
       <ul className='hidden md:flex text-xl'>
+        <li className='p-5'>Início</li>
         <li className='p-5'>Clínica</li>
         <li className='p-5'>Serviços</li>
         <li className='p-5'>Médicos</li>
