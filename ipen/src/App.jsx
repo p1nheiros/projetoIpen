98b6d817d71d336edfacdba0b3b120dca7b55d6e
p1nheiros <pinheiros.dev@gmail.com>
2023-06-12 21:01:44 -0300

    atualização medicos e correção navbar fixa

--- ipen/src/App.jsx
+++ ipen/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Home } from './components/Home'
-import { Navbar } from './components/Navbar'
+import { Home } from './components/Home/Home'
+import { Navbar } from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import Galeria from './components/Galeria/Galeria'
 
 import './styles/global.css'
+import { Medicos } from './components/Medicos/Medicos'
 
 function App() {
   return (
     <div className='font-poppins'>
       <Navbar/>
       <Home/>
+      <Medicos />
       <Galeria />
       <Footer/>
     </div>
