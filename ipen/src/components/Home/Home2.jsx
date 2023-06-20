3f7d8de655b65570e733c0ac89d62251602de511
p1nheiros <pinheiros.dev@gmail.com>
2023-06-19 21:07:49 -0300

    adicionando pagina para refazer o home

---
+++ ipen/src/components/Home/Home2.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Home2 = () => {
+    return (
+        <div className="bg-[url('../img/imageBG2.png')] bg-cover bg-center bg-no-repeat min-h-screen flex-col justify-center pt-[60px] pb-[40px] select-none">
+        </div>
+    )
+}
